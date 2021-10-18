c147ad9056c7e6bc1a0aef90ba3f3867ac27f1f1
Ester <ester_sgomes@hotmail.com>
2021-10-17 21:27:31 -0300

    erro tinder card, rever

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './component/Header';
+import DatingCards from './component/DatingCards';
+import Swipebuttons from './component/SwipeButtons';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <DatingCards /> */}
+      <Swipebuttons />
+    </div>
+  );
+}
+
+export default App;
